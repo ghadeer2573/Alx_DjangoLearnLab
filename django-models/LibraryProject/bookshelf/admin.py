0d50ee9d684728b3041b50da2165c5aa9a6afd78
Ghadeer <ghadeer.raafat@gmail.com>
2025-07-20 19:04:26 +0300

    super

---
+++ django-models/LibraryProject/bookshelf/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Book
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')  # Columns shown
+    list_filter = ('publication_year',)                     # Sidebar filters
+    search_fields = ('title', 'author')                     # Search box
+
