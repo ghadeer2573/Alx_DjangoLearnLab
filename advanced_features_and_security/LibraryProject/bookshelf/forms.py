fa39a2047f00d642e3eec950e92d158f8c8a61a0
Ghadeer <ghadeer.raafat@gmail.com>
2025-07-27 21:06:11 +0300

    super

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -3,3 +3,11 @@ from .models import Book
 
 class BookSearchForm(forms.Form):
     query = forms.CharField(label='Search', max_length=100)
+
+from django import forms
+from .models import Book
+
+class ExampleForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'published_date']
